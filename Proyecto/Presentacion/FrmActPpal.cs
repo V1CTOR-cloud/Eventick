56cfc4a1a162cc0af069416bc2d75e4a3914b58c
Claudiu Andrei Nechitescu <clne@floridauniversitaria.es>
2021-05-20 20:05:28 +0200

    Borrando evento prueba caragar actividad

--- Proyecto/Presentacion/FrmActPpal.cs
+++ Proyecto/Presentacion/FrmActPpal.cs
@@ -156,43 +156,6 @@ namespace Eventick
             }
         }
 
-        private void toggPruebaPaneles_CheckedChanged(object sender, EventArgs e)
-        {
-            Panel panelNoticia = new Panel() { Name = "panelNoticia", BackColor = Color.Transparent, Size = new Size(850, 145) };
-            flpNoticias.Controls.Add(panelNoticia);
-            PictureBox picNoticia = new PictureBox() { Name = "picNoticia", Image = Image.FromFile(@"..\..\..\Iconos\yo.jpg"), Size = new Size(241, 135), SizeMode = PictureBoxSizeMode.Normal };
-            panelNoticia.Controls.Add(picNoticia);
-            LinkLabel llblTitulo = new LinkLabel() { Name="llblTitulo", Location= new Point(261, 2), Text="Hola" };
-            panelNoticia.Controls.Add(llblTitulo);
-            Label lblLocalidad = new Label() { Name="lblLocalidad", Location= new Point(264, 24), Text="Casa", Font=new Font("Microsoft Sans Serif", 12,FontStyle.Bold) };
-            panelNoticia.Controls.Add(lblLocalidad);
-            Panel panelInfo = new Panel() { Name="panelInfo", BackColor=Color.Transparent,Size = new Size(588, 25), Location=new Point(276, 46) };
-            panelNoticia.Controls.Add(panelInfo);
-            Label lblInfo1 = new Label() { Name = "lblInfo1", AutoSize=true, Dock = DockStyle.Left, Text = "466m" };
-            panelInfo.Controls.Add(lblInfo1);
-            Label lblInfo2 = new Label() { Name = "lblInfo2", AutoSize = true, Dock = DockStyle.Left, Text = "|" };
-            panelInfo.Controls.Add(lblInfo2);
-            Label lblInfo3 = new Label() { Name = "lblInfo3", AutoSize = true, Dock = DockStyle.Left, Text = "Circular" };
-            panelInfo.Controls.Add(lblInfo3);
-            Label lblInfo4 = new Label() { Name = "lblInfo4", AutoSize = true, Dock = DockStyle.Left, Text = "|" };
-            panelInfo.Controls.Add(lblInfo4);
-            Label lblInfo5 = new Label() { Name = "lblInfo5", AutoSize = true, Dock = DockStyle.Left, Text = "Media" };
-            panelInfo.Controls.Add(lblInfo5);
-            Label lblInfo6 = new Label() { Name = "lblInfo6", AutoSize = true, Dock = DockStyle.Left, Text = "|" };
-            panelInfo.Controls.Add(lblInfo6);
-            Label lblInfo7 = new Label() { Name = "lblInfo7", AutoSize = true, Dock = DockStyle.Left, Text = "4h" };
-            panelInfo.Controls.Add(lblInfo7);
-            Label lblInfo8 = new Label() { Name = "lblInfo8", AutoSize = true, Dock = DockStyle.Left, Text = "|" };
-            panelInfo.Controls.Add(lblInfo8);
-            Label lblInfo9 = new Label() { Name = "lblInfo9", AutoSize = true, Dock = DockStyle.Left, Text = "12 km" };
-            panelInfo.Controls.Add(lblInfo9);
-            PictureBox picFavorito = new PictureBox() { Name = "picFavorito", Cursor=Cursors.Hand, Image = favoritovacio, Size = new Size(35, 35), Location = new Point(261, 100), SizeMode = PictureBoxSizeMode.Zoom };
-            panelNoticia.Controls.Add(picFavorito);
-            PictureBox picCompartir = new PictureBox() { Name="picCompartir", Image=Image.FromFile(@"..\..\..\Iconos\compartir.png"),Size=new Size(35,35), Location= new Point(300,100), SizeMode = PictureBoxSizeMode.Zoom };
-            panelNoticia.Controls.Add(picCompartir);
-            picFavorito.Click += new EventHandler(picFavorito_Click);
-            
-        }
 
         Color colorBotonesActivos = Color.FromArgb(230, 240, 229);
         private void btnActividad_Click(object sender, EventArgs e)
